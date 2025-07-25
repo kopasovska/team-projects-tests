40e1cc3286e499b2069c67a7deb51ef8748f2866
kopasovska <v.kopasovska@gmail.com>
2025-07-25 07:46:26 +0200

    Edit users model

--- src/db/models/users.js
+++ src/db/models/users.js
@@ -2,9 +2,14 @@ import { model, Schema } from 'mongoose';
 
 const usersSchema = new Schema(
   {
+    // name: { type: String, required: true },
+    // email: { type: String, required: true, unique: true },
+    // password: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatarUrl: {},
+    articlesAmount: {},
   },
   {
     timestamps: true,
